d81442d4c1742a7d53c4fed3b3e99b96a55a5ee2
pastilhex <ivodanielmarques@gmail.com>
2024-05-29 10:41:41 +0100

    Wordpress without log erros

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,17 +3,16 @@
 if [ ! -d "/var/lib/mysql/my_db" ]; then
 	mysqld_safe --user=mysql --datadir=/var/lib/mysql --pid-file=/var/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock --log-error=/var/log/mysql/error.log &
 
-
 	sleep 2
 
     # Setup user and database
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" -h localhost
-    mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -h localhost
-    mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -h localhost
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" -h localhost
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" -h localhost
+    mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -h localhost
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -h localhost
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" -h localhost
     
     # Shutdown the server
-    mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
 
 exec mysqld --bind-address=0.0.0.0 --user=mysql
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
-export SQL_PASSWORD=$(cat /run/secrets/db_password)
-export SQL_DATABASE=$(cat /run/secrets/db_name)
-export SQL_HOST=$(cat /run/secrets/db_host)
-export WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
-export WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
-
-sed -i 's|listen = /run/php/php8.1-fpm.sock|listen = wordpress:9000|' /etc/php/8.1/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = wordpress:9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir -p /run/php/
+chown www-data:www-data /run/php
 
-if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+if [ ! -f /var/www/html/ialves-m/wordpress/wp-config.php ]; then
 
     echo "Installing WP-CLI... wait a moment"
     wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp && \
     chmod +x /usr/local/bin/wp
 
-    mkdir -p /var/www/html/wordpress
-    cd /var/www/html/wordpress
-    wp core download --allow-root
+    mkdir -p /var/www/html/ialves-m/wordpress
+    cd /var/www/html/ialves-m/wordpress
+    
+	wget -q https://wordpress.org/latest.tar.gz
+	tar -xvf latest.tar.gz --strip-components=1
+	
+	# wp core download --allow-root
 
     sleep 2
     
-    wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
+    # wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
+    wp config create --dbname=my_db --dbuser=my_user --dbpass=my_pass --dbhost=mariadb --path=/var/www/html/ialves-m/wordpress --allow-root
     wp core install --url="$WP_URL" \
 	--title="$WP_TITLE" \
 	--admin_user="$WP_ADMIN_USER" \
@@ -37,6 +37,9 @@ else
     echo "Wordpress already installed and configured"
 fi
 
-echo "Access WordPress site here: https://ialves-m.42.fr"
+# echo "Access WordPress site here: https://ialves-m.42.fr"
+# service php7.4-fpm stop
 
-exec php-fpm8.1 -F
+exec /usr/sbin/php-fpm7.4 -F
+
+# sleep 330
